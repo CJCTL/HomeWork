78ec70817fbe3c700b0f1a42bca0831309cd87a6
CJCTL <chernogor13@protonmail.com>
2025-10-13 19:27:50 +0300

    HelloWorld:d32

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        String name = "Vanno";
+        int age = 20;
+        double height = 1.88;
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is student: " + isStudent);
+    }
+}
---
+++ Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age < 18) {
+            System.out.println("Доступ заборонено");
+        } else {
+            System.out.println("Доступ дозволено");
+        }
+    }
+}
