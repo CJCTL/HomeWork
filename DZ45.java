275526dcb30ae196b337385d5805329bd19175e1
CJCTL <chernogor13@protonmail.com>
2025-10-23 19:18:57 +0300

    DZ4END

---
+++ DZ45.java
@@ -0,0 +1,15 @@
+package poza;
+
+public class DZ45 {
+    public static void main(String[] args) {
+        String expected = "Login successful";
+
+        String actual = "Login successful";
+
+        if (expected.equals(actual)) {
+            System.out.println("Тест пройден ");
+        } else {
+            System.out.println("Тест не пройдено");
+        }
+    }
+}
